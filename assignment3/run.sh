64cb044a249fcc7513378e138e2acf44016695b8
Trevor Bramwell <trevor@bramwell.net>
2015-04-29 17:17:40 -0700

    Project 3 - Code Commit

---
+++ assignment3/run.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+threads=(1 2 4 8 16)
+padding=(0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16)
+
+[[ -d data ]] || mkdir data
+
+for T in ${threads[@]}
+do
+    for N in ${padding[@]}
+    do
+        make -s NUMT=${T} NUM=${N}
+        ./false-sharing >> data/padding${T}.dat
+    done
+done
---
+++ assignment3/src/main.cc
@@ -0,0 +1,66 @@
+//
+// Trevor Bramwell
+// CS475 - Assignment 3
+//
+// File: main.cc
+//
+
+// False Sharing
+
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <omp.h>
+
+using std::cout;
+
+#ifndef NUM
+#define NUM       0
+#endif
+
+#ifndef NUMT
+#define NUMT      4
+#endif
+
+struct s
+{
+    float value;
+    int pad[NUM];
+} Array[4];
+
+
+int
+main(int argc, char* argv[])
+{
+    unsigned int someBigNumber = 1000000000;    // if > 4B, use "long unsigned int"
+
+    omp_set_num_threads(NUMT);
+
+    double time0 = omp_get_wtime();
+
+    #pragma omp parallel for
+    for( int i = 0; i < 4; i++ )
+    {
+        for( unsigned int j = 0; j < someBigNumber; j++ )
+        {
+            Array[i].value = Array[i].value + 2.0;
+        }
+    }
+
+    double time1 = omp_get_wtime();
+    double timedelta = (time1-time0);
+
+    // Performance
+
+#if NUM == 0
+    cout << "NUM\tNUMT\tPERF\tTIME" << "\n";
+#endif
+    cout << NUM << "\t";
+    cout << NUMT << "\t";
+    cout << ((float)someBigNumber*4.0)/timedelta/10000000.0 << "\t";
+    cout << timedelta << "s";
+    cout << "\n";
+
+    return 0;
+}
